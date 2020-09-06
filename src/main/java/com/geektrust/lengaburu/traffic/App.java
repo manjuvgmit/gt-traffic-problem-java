fa62f98ccabd511f8d9307805f2e82df10a68fad
Manjunatha Veeranna <manjuv.gmit@gmail.com>
2020-09-06 18:27:39 +0530

    Refactored

--- src/main/java/com/geektrust/lengaburu/traffic/App.java
+++ src/main/java/com/geektrust/lengaburu/traffic/App.java
@@ -3,14 +3,23 @@
  */
 package com.geektrust.lengaburu.traffic;
 
-import com.geektrust.lengaburu.traffic.entities.*;
+import com.geektrust.lengaburu.traffic.entities.InputParameters;
+import com.geektrust.lengaburu.traffic.entities.Orbit;
+import com.geektrust.lengaburu.traffic.entities.Vehicles;
+import com.geektrust.lengaburu.traffic.entities.Weather;
 import com.geektrust.lengaburu.traffic.utils.MiscUtils;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Comparator;
+import java.util.Map;
 
 import static com.geektrust.lengaburu.traffic.entities.Weather.ImpactOnCraters.DECREASE;
+import static java.util.Arrays.stream;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
 
+/**
+ * Main app and entry point for the problem
+ */
 public class App {
 
     public static void main(String[] args) throws Exception {
@@ -27,57 +36,83 @@ public class App {
     public String determineVehicleAndRoute(String argument) {
         try {
             InputParameters inputParameters = InputParameters.parse(argument);
-            List<ResultData> resultDataStream = Arrays.stream(Vehicles.values())
+
+            Map<Vehicles, Map<Orbit, Double>> weatherFilteredAndTimeCalculatedData = stream(Vehicles.values())
+                    // Filter all vehicles not supported for the weather first
                     .filter(vehicles -> vehicles.getAllowedWeather().contains(inputParameters.getWeather()))
-                    .map(vehicle -> calculateTimeTakenForEachVehicle(vehicle, inputParameters))
-                    .collect(Collectors.toList());
-            //System.out.println("time calculated and filtered : " + resultDataStream.toString());
-            Map<Orbit, ResultData> resultMap = new HashMap<>();
-            Arrays.stream(Orbit.values()).forEach(orbit -> resultMap.put(orbit, resultDataStream.stream().min(Comparator.comparing(r -> r.getTimeEntries().get(orbit))).orElse(null)));
-            //System.out.println("Filtered on orbits : " + resultMap.toString());
-            Map.Entry<Orbit, ResultData> finalResult = resultMap.entrySet().stream().min(Comparator.comparing(t -> t.getValue().getTimeEntries().get(t.getKey()))).orElse(null);
-            //System.out.println("Final Result : " + finalResult.toString());
-            return finalResult.getValue().getVehicle().getShortName() + " " + finalResult.getKey().name();
+                    // Calculate time would take for each vehicle for all orbits available. i.e. ORBIT1, ORBIT2
+                    .collect(toMap(identity(), vehicles -> calculateTimeTakenForEachVehicle(vehicles, inputParameters)));
+
+            return stream(Orbit.values())
+                    // Find out faster vehicle for each orbit
+                    .collect(toMap(identity(), orbit -> weatherFilteredAndTimeCalculatedData.entrySet().stream().min(Comparator.comparing(r -> r.getValue().get(orbit))).get()))
+                    // Find out fastest vehicle among faster vehicle for each orbit
+                    .entrySet().stream().min(Comparator.comparing(t -> t.getValue().getValue().get(t.getKey())))
+                    // format data in required way
+                    .map(entry -> entry.getValue().getKey().getShortName() + " " + entry.getKey().name())
+                    .orElse(null);
+
         } catch (Exception ex) {
             return ex.getMessage();
         }
     }
 
-    private ResultData calculateTimeTakenForEachVehicle(Vehicles vehicle, InputParameters inputParameters) {
-        final ResultData resultData = new ResultData(vehicle);
-        Arrays.stream(Orbit.values()).forEach(orbit ->
-                resultData.getTimeEntries().put(orbit, getTimeTakenForVehicleForAnOrbit(orbit, vehicle, inputParameters))
-        );
-        return resultData;
+    /**
+     * Calculates time taken by each vehicle for all available orbits considering the weather conditions.
+     * @param @param vehicle vehicle chosen for an orbit to travel
+     * @param inputParameters input parameters passed from command line consisting of weather and traffic parameters
+     * @return a map containing time taken by the vehicle for each orbit in minutes
+     */
+    private Map<Orbit, Double> calculateTimeTakenForEachVehicle(Vehicles vehicle, InputParameters inputParameters) {
+        return stream(Orbit.values()).collect(toMap(identity(), orbit -> getTimeTakenForVehicleForAnOrbit(orbit, vehicle, inputParameters)));
     }
 
+    /**
+     * Calculates overall time taken by a vehicle to travel the given orbit's distance considering potholes delay as well.
+     * @param orbit orbit being considered for a vehicle to travel
+     * @param vehicle vehicle chosen for an orbit to travel
+     * @param inputParameters input parameters passed from command line consisting of weather and traffic parameters
+     * @return time taken in minutes for a vehicle to travel the passed in Orbit's distance without delay from potholes
+     */
     private double getTimeTakenForVehicleForAnOrbit(Orbit orbit, Vehicles vehicle, InputParameters inputParameters) {
         return (getStraightUpTimeTakenInHours(orbit, vehicle, inputParameters) * 60.0)
                 + getDelayDueToCratersInMinutes(orbit, vehicle, inputParameters);
     }
 
+    /**
+     * Calculates time taken by a vehicle to travel the given orbit's distance without considering potholes delay
+     * @param orbit orbit being considered for a vehicle to travel
+     * @param vehicle vehicle chosen for an orbit to travel
+     * @param inputParameters input parameters passed from command line consisting of weather and traffic parameters
+     * @return time taken in minutes for a vehicle to travel the passed in Orbit's distance without delay from potholes
+     */
     private double getStraightUpTimeTakenInHours(Orbit orbit, Vehicles vehicle, InputParameters inputParameters) {
         return 1.0 * orbit.getDistance().getValue()
                 / Math.min(vehicle.getSpeed().getValue(), inputParameters.getOrbitsCurrentTrafficSpeeds().get(orbit).getValue());
     }
 
+    /**
+     * Calculates delay caused for any vehicle type when going over an orbit considering weather conditions
+     * @param orbit orbit being considered for a vehicle to travel
+     * @param vehicle vehicle chosen for an orbit to travel
+     * @param inputParameters input parameters passed from command line consisting of weather and traffic parameters
+     * @return time delay in minutes introduced for a vehicle to travel a given Orbit's distance considering weather condition
+     */
     private double getDelayDueToCratersInMinutes(Orbit orbit, Vehicles vehicle, InputParameters inputParameters) {
         return vehicle.getTimeToCrossCrater() * getTotalNoOfCraters(orbit, inputParameters);
     }
 
     /**
      * Returns either reduced or increased number of craters depending on weather and orbit.
-     * Result is case to int to keep craters count in realistic.
-     *
+     * Result has been cast to int to keep craters count in integers
      * @param inputParameters input parameters
      * @return increased or decreased count of craters for the given weather and orbit
      */
     private int getTotalNoOfCraters(Orbit orbit, InputParameters inputParameters) {
         Weather currentWeather = inputParameters.getWeather();
         // Total no of craters = craters + craters created due to season
-        return (int) (
-                orbit.getNoOfCraters()
-                        * (1 + ((currentWeather.getImpactOnCraters() == DECREASE ? -1 : 1)) * currentWeather.getRateOfImpactOnCraters())
+        return (int) (orbit.getNoOfCraters()
+                * (1 + ((currentWeather.getImpactOnCraters() == DECREASE ? -1 : 1)) * currentWeather.getRateOfImpactOnCraters())
         );
     }
 }
